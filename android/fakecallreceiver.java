a65def348467b9636d47187324201a73a8fd53e2
kevinclient <36801077+kevinclient@users.noreply.github.com>
2018-02-25 03:22:45 +0530

    Create fakecallreceiver.java
    java call receiver

---
+++ android/fakecallreceiver.java
@@ -0,0 +1,15 @@
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
+
