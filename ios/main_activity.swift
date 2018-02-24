5fb7ec1194e65afde04aaa74ab9fb59440e3e74e
kevinclient <36801077+kevinclient@users.noreply.github.com>
2018-02-25 03:20:44 +0530

    Update and rename main_activity.java to main_activity.swift

---
+++ ios/main_activity.swift
@@ -0,0 +1,19 @@
+class StudDetails {
+   var stname: String!
+   var mark: Int!
+   
+   init(stname: String, mark: Int) {
+      self.stname = stname
+      self.mark = mark
+   }
+   deinit {
+      print("Deinitialized \(self.stname)")
+      print("Deinitialized \(self.mark)")
+   }
+}
+
+let stname = "Swift 4"
+let mark = 98
+
+print(stname)
+print(mark)
